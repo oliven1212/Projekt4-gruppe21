550fecf98612d85c35c10b832a4c190cc4fdf150
Valdemar <pizza27@live.dk>
2024-03-11 11:55:42 +0100

    General header

--- javascript/javascriptgeneral.js
+++ javascript/javascriptgeneral.js
@@ -1,1 +1,5 @@
-document.getElementById("header").innerHTML = '<h1>Skole begivenheder</h1>'
+document.getElementById("header").innerHTML = '<a href="index.html" class="highlight"><p>Forside</p></a>';
+document.getElementById("header").innerHTML += '<a href="kort.html" class="highlight"><p>Kort</p></a>';
+document.getElementById("header").innerHTML += '<a href="index.html" class="headerpicture"><img src="pictures/general/logo.png" alt="logo"></a>';
+document.getElementById("header").innerHTML += '<a href="events.html" class="highlight"><p>Begivenheder</p></a>';
+document.getElementById("header").innerHTML += '<a href="about.html" class="highlight"><p>Kontakt</p></a>';
