a72b5d889c787c7c5a717ace849f56cc0bf77332
Valdemar <pizza27@live.dk>
2024-03-19 10:59:12 +0100

    .

--- javascript/javascriptgeneral.js
+++ javascript/javascriptgeneral.js
@@ -1,8 +1,10 @@
-document.getElementById("header").innerHTML = '<a href="index.html" class="highlight"><p>Forside</p></a>';
-document.getElementById("header").innerHTML += '<a href="kort.html" class="highlight"><p>Kort</p></a>';
+document.getElementById("header").innerHTML  = '<a href="index.html" class="highlight"><p>Forside</p></a>';
+document.getElementById("header").innerHTML += '<a href="effort.html" class="highlight"><p>Indsats</p></a>';
 document.getElementById("header").innerHTML += '<a href="index.html" class="headerpicture"><img src="pictures/general/logo.png" alt="logo"></a>';
 document.getElementById("header").innerHTML += '<a href="events.html" class="highlight"><p>Begivenheder</p></a>';
 document.getElementById("header").innerHTML += '<a href="about.html" class="highlight"><p>Om os</p></a>';
 
 
-document.getElementById("footer").innerHTML = '<div><h2>Kontakt information</h2><p>Email: info@dinstilderessmil.com</p><p>Telefon: +45 23 42 45 21</p></div><div><h2>Åbningstider</h2><p>Mandag-fredag: 9:00 - 17:00</p><p>Lørdag-søndag: Lukket</p></div><div><h2>Følg os på sociale medier</h2><a title="Facebook" href="https://www.google.com/"><i class="fa-brands fa-facebook"></i></a><a title="Instagram" href="https://www.google.com/"><i class="fa-brands fa-square-instagram"></i></a><a title="Linkind" href="https://www.google.com/"><i class="fa-brands fa-linkedin"></i></a><a title="Twitter" href="https://www.google.com/"><i class="fa-brands fa-twitter"></i></a><a title="Youtube" href="https://www.google.com/"><i class="fa-brands fa-youtube"></i></a></div>';
+document.getElementById("footer").innerHTML  = '<div><h2>Kontakt information</h2><p>Email: info@dinstilderessmil.com</p><p>Telefon: +45 23 42 45 21</p></div>';
+document.getElementById("footer").innerHTML += '<div><h2>Åbningstider</h2><p>Mandag-fredag: 9:00 - 17:00</p><p>Lørdag-søndag: Lukket</p></div>';
+document.getElementById("footer").innerHTML += '<div><h2>Følg os på sociale medier</h2><a href="https://www.google.com/"><i class="fa-brands fa-facebook"></i></a><a href="https://www.google.com/"><i class="fa-brands fa-square-instagram"></i></a><a href="https://www.google.com/"><i class="fa-brands fa-linkedin"></i></a><a href="https://www.google.com/"><i class="fa-brands fa-twitter"></i></a><a href="https://www.google.com/"><i class="fa-brands fa-youtube"></i></a></div>';
