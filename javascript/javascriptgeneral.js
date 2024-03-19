98289ae0bef7e7e0d856e9309168adb06039d55d
Valdemar <pizza27@live.dk>
2024-03-19 12:47:22 +0100

    Update javascriptgeneral.js

--- javascript/javascriptgeneral.js
+++ javascript/javascriptgeneral.js
@@ -6,5 +6,5 @@ document.getElementById("header").innerHTML += '<a href="about.html" class="high
 
 
 document.getElementById("footer").innerHTML  = '<div><h2>Kontakt information</h2><p>Email: info@dinstilderessmil.com</p><p>Telefon: +45 23 42 45 21</p></div>';
-document.getElementById("footer").innerHTML += '<div><h2>Åbningstider</h2><p>Mandag-fredag: 9:00 - 17:00</p><p>Lørdag-søndag: Lukket</p></div>';
+document.getElementById("footer").innerHTML += '<div><h2>Kontakt åbningstider</h2><p>Mandag-fredag: 9:00 - 17:00</p><p>Lørdag-søndag: Lukket</p></div>';
 document.getElementById("footer").innerHTML += '<div><h2>Følg os på sociale medier</h2><a href="https://www.google.com/"><i class="fa-brands fa-facebook"></i></a><a href="https://www.google.com/"><i class="fa-brands fa-square-instagram"></i></a><a href="https://www.google.com/"><i class="fa-brands fa-linkedin"></i></a><a href="https://www.google.com/"><i class="fa-brands fa-twitter"></i></a><a href="https://www.google.com/"><i class="fa-brands fa-youtube"></i></a></div>';
