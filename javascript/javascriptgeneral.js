3e880382b365536ca10ba3c07b70d87ee3b21150
Valdemar <pizza27@live.dk>
2024-04-04 12:20:45 +0200

    h

--- javascript/javascriptgeneral.js
+++ javascript/javascriptgeneral.js
@@ -42,4 +42,4 @@ document.getElementById("footer").innerHTML  = '<div><h2>Kontakt information</h2
 document.getElementById("footer").innerHTML += '<div><h2>Kontakt åbningstider</h2><p>Mandag-fredag: 9:00 - 17:00</p><p>Lørdag-søndag: Lukket</p></div>';
 document.getElementById("footer").innerHTML += '<div><h2>Følg os på sociale medier</h2><a href="https://www.google.com/"><i class="fa-brands fa-facebook"></i></a><a href="https://www.google.com/"><i class="fa-brands fa-square-instagram"></i></a><a href="https://www.google.com/"><i class="fa-brands fa-linkedin"></i></a><a href="https://www.google.com/"><i class="fa-brands fa-twitter"></i></a><a href="https://www.google.com/"><i class="fa-brands fa-youtube"></i></a></div>';
 
-document.getElementById("banner").innerHTML = '<img src="pictures/general/Banner.png" alt="Banner">';
+document.getElementById("banner").innerHTML = '<img src="pictures/general/Banner.jpg" alt="Banner">';
