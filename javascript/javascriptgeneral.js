3fe668ff7bedc581ee9c9b0357728678c677a86b
Valdemar <pizza27@live.dk>
2024-04-04 10:28:10 +0200

    r

--- javascript/javascriptgeneral.js
+++ javascript/javascriptgeneral.js
@@ -1,8 +1,30 @@
-document.getElementById("header").innerHTML  = '<a href="index.html" class="highlight"><p>Forside</p></a>';
-document.getElementById("header").innerHTML += '<a href="effort.html" class="highlight"><p>Indsats</p></a>';
-document.getElementById("header").innerHTML += '<a href="index.html" class="headerpicture"><img src="pictures/general/logo.png" alt="logo"></a>';
-document.getElementById("header").innerHTML += '<a href="events.html" class="highlight"><p>Begivenheder</p></a>';
-document.getElementById("header").innerHTML += '<a href="about.html" class="highlight"><p>Om os</p></a>';
+function pcheader(){
+    document.getElementById("header").innerHTML  = '<a href="index.html" class="highlight"><p>Forside</p></a>';
+    document.getElementById("header").innerHTML += '<a href="effort.html" class="highlight"><p>Indsats</p></a>';
+    document.getElementById("header").innerHTML += '<a href="index.html" class="headerpicture"><img src="pictures/general/logo.png" alt="logo"></a>';
+    document.getElementById("header").innerHTML += '<a href="events.html" class="highlight"><p>Begivenheder</p></a>';
+    document.getElementById("header").innerHTML += '<a href="about.html" class="highlight"><p>Om os</p></a>';
+}
+function phoneheader(){  
+    document.getElementById("header").innerHTML = '<a href="index.html" class="burperpic"><img src="pictures/general/logo.png" alt="logo"></a>';
+    document.getElementById("header").innerHTML+= '<a href="javascript:void(0);" onclick="onresize()" class="navbarbutton"><i class="fa fa-bars"></i></a>';
+
+}
+
+function onresize () {
+    console.log(window.innerWidth);
+    if(window.innerWidth > 1500){
+        pcheader();
+    }else{
+        phoneheader();
+    }
+ }
+
+
+ onresize();
+ window.addEventListener("resize", onresize);
+
+
 
 
 document.getElementById("footer").innerHTML  = '<div><h2>Kontakt information</h2><p>Email: info@dinstilderessmil.com</p><p>Telefon: +45 23 42 45 21</p></div>';
