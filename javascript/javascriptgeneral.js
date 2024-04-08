cb8afbe0d8a045bcb5757f08055b220c95cf8e2e
Valdemar <pizza27@live.dk>
2024-04-08 10:20:52 +0200

    i

--- javascript/javascriptgeneral.js
+++ javascript/javascriptgeneral.js
@@ -1,6 +1,6 @@
 function pcheader(){
     document.getElementById("header").innerHTML  = '<a href="index.html" class="highlight"><p>Forside</p></a>';
-    document.getElementById("header").innerHTML += '<a href="effort.html" class="highlight"><p>Kort</p></a>';
+    document.getElementById("header").innerHTML += '<a href="map.html" class="highlight"><p>Kort</p></a>';
     document.getElementById("header").innerHTML += '<a href="index.html" class="headerpicture"><img src="pictures/general/logo.png" alt="logo"></a>';
     document.getElementById("header").innerHTML += '<a href="events.html" class="highlight"><p>Begivenheder</p></a>';
     document.getElementById("header").innerHTML += '<a href="about.html" class="highlight"><p>Om os</p></a>';
