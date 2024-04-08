970ab597327cedef6f27d9a47879fcd4fbfb46f6
Valdemar <pizza27@live.dk>
2024-04-08 12:09:50 +0200

    s

--- javascript/javascriptgeneral.js
+++ javascript/javascriptgeneral.js
@@ -41,6 +41,6 @@ function onresize () {
 document.getElementById("footer").innerHTML  = '<div><h2>Kontakt information</h2><p>Email: info@dinstilderessmil.com</p><p>Telefon: +45 23 42 45 21</p></div>';
 document.getElementById("footer").innerHTML += '<div><h2>Kontakt åbningstider</h2><p>Mandag-fredag: 9:00 - 17:00</p><p>Lørdag-søndag: Lukket</p></div>';
 document.getElementById("footer").innerHTML += '<div></div><div></div>';
-document.getElementById("footer").innerHTML += '<div><h2>Følg os på sociale medier</h2><a href="https://www.google.com/"><i class="fa-brands fa-facebook"></i></a><a href="https://www.google.com/"><i class="fa-brands fa-square-instagram"></i></a><a href="https://www.google.com/"><i class="fa-brands fa-linkedin"></i></a><a href="https://www.google.com/"><i class="fa-brands fa-twitter"></i></a><a href="https://www.google.com/"><i class="fa-brands fa-youtube"></i></a></div>';
+document.getElementById("footer").innerHTML += '<div><h2>Følg os på sociale medier</h2><a href="https://www.facebook.com/roedekors/"><i class="fa-brands fa-facebook"></i></a><a href="https://www.instagram.com/rodekorsdk"><i class="fa-brands fa-square-instagram"></i></a><a href="https://www.linkedin.com/company/danish-red-cross/"><i class="fa-brands fa-linkedin"></i></a><a href="https://twitter.com/danskrodekors"><i class="fa-brands fa-twitter"></i></a><a href="https://www.youtube.com/channel/UCGSDJNCcQNsLJKP6PhF8kvg"><i class="fa-brands fa-youtube"></i></a></div>';
 
 document.getElementById("banner").innerHTML = '<img src="pictures/general/Banner.jpg" alt="Banner">';
--- javascript/javascriptmap.js
+++ javascript/javascriptmap.js
@@ -1,13 +1,13 @@
-const id = ["12345"];
-const firstlocation = ["Frederiksværk pakkecenter"];
-const firstdate = ["17. marts",];
-const firsttime = ["17:32",];
-const secondlocation = ["Kastrup lufthavn"];
-const seconddate = ["27. marts"];
-const secondtime = ["14:40"];
-const thirdlocation = ["Uganda"];
-const thirddate = ["31. marts"];
-const thirdtime = ["10:22"];
+const id = [12345,1,2];
+const firstlocation = ["Frederiksværk pakkecenter","Odense pakkecenter","Frederiksværk pakkecenter"];
+const firstdate = ["17. Marts", "13. Maj", "14. September"];
+const firsttime = ["17:32", "14:50","10:50"];
+const secondlocation = ["Kastrup lufthavn","Kastrup lufthavn", "Kastrup lufthavn"];
+const seconddate = ["27. Marts", "3. Oktober", "15 Janaur"];
+const secondtime = ["14:40", "11:22", "9:22"];
+const thirdlocation = ["Uganda", "Uganda", "Uganda"];
+const thirddate = ["31. marts", "22. august", "19. maj"];
+const thirdtime = ["10:22", "12:11", "13:53"];
 
 function trackShipment() {
     var trackingNumber = document.getElementById('trackingNumber').value;
@@ -32,7 +32,7 @@ function trackShipment() {
     var trackingNumber = document.getElementById('trackingNumber').value;
 
     // Simulere en anmodning til serveren for at få forsendelsesdetaljer
-    if (trackingNumber === id[0]) {
+    if (trackingNumber == id[0]) {
         document.getElementById('trackingResult').style.display = 'block';
         // Vis satisfactionBox, når det ønskede sporingsnummer er indtastet
         document.querySelector('.satisfactionBox').style.display = 'block';
