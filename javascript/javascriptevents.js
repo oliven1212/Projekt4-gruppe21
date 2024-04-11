17b622e6961c93caa69b3cfff7a30e50605c9050
JeppeDahlbom <jeppedahlbom@hotmail.com>
2024-04-11 12:47:52 +0200

    s

--- javascript/javascriptevents.js
+++ javascript/javascriptevents.js
@@ -6,4 +6,4 @@ const filename = ["cbs_koebenhavn","dtu_odense","it_universitet_koebenhavn","koe
 
 for (let i = 0; i < uniname.length; i++) {
 document.getElementById("galleri").innerHTML  += '<div class="box"><img src="pictures/events/'+filename[i]+'.png" alt="united"><div class="img-text"><h3>'+uniname[i]+'</h3><p>'+unilocation[i]+'</p><p>'+eventtime[i]+'</p></div><div class="cover"><h3>'+uniname[i]+'</h3><p>'+unilocation[i]+'</p><p>'+eventtime[i]+'</p><p>'+eventinfo[i]+'</p></div></div>';
-}
+} 
