7caf34ae9d18c17bbc2416109597b3bb28d454e5
Valdemar <pizza27@live.dk>
2024-04-11 10:32:27 +0200

    Update javascriptevents.js

--- javascript/javascriptevents.js
+++ javascript/javascriptevents.js
@@ -1,4 +1,4 @@
-const uniname = ["Ordrup gymnasium","UCL Erhvers akademi","Makurs plads"];
+const uniname = ["Copenhagen Business School","Dansk teknisk universitet","IT University of Copenhagen","Copenhagen University","Roskilde University","University of Southern Denmark","Aalborg universitet","Aarhus Universitet"];
 const unilocation = ["Solbjerg Pl. 3, 2000 Frederiksberg","Rued Langgaards Vej 7, 2300 København","Nørregade 10, 1172 København","Universitetsvej 1, 4000 Roskilde","Campusvej 55, 5000 Odense","Fredrik Bajers Vej 7K, 9220 Aalborg Øst","Nordre Ringgade 1, 8000 Aarhus C"];
 const eventtime = ["Ordrup gymnasium","UCL Erhvers akademi","Makurs plads"];
 const eventinfo = ["Ordrup gymnasium","UCL Erhvers akademi","Makurs plads"];
