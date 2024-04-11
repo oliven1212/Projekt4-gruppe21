4abbea0814b6556467c4e1d5241a614a1111f569
JeppeDahlbom <jeppedahlbom@hotmail.com>
2024-04-11 11:35:02 +0200

    dato

--- javascript/javascriptevents.js
+++ javascript/javascriptevents.js
@@ -1,6 +1,6 @@
 const uniname = ["Copenhagen Business School","Dansk teknisk universitet","IT University of Copenhagen","Copenhagen University","Roskilde University","University of Southern Denmark","Aalborg universitet","Aarhus Universitet"];
 const unilocation = ["Solbjerg Pl. 3, 2000 Frederiksberg","Anker Engelunds Vej 1, Bygning 101A, 2800 Kongens Lyngby","Rued Langgaards Vej 7, 2300 København","Nørregade 10, 1172 København","Universitetsvej 1, 4000 Roskilde","Campusvej 55, 5230 Odense","Fredrik Bajers Vej 7K, 9220 Aalborg Øst","Nordre Ringgade 1, 8000 Aarhus C"];
-const eventtime = ["Ordrup gymnasium","DTU: Danmarks Tekniske Universitet ","IT-Universitetet", "Københavns Universitet","RUC:Universitet","SDU:Syddansk Universitet","AAU:Aalborg Universitet", "AU:Aarhus Universitet"];
+const eventtime = ["21/02-2024 -> 28/02-2024","02/01-2024 -> 09/01-2024","21/04-2024 -> 28/04-2024", "21/05-2024 -> 28/05-2024","21/06-2024 -> 28/06-2024","21/07-2024 -> 28/07-2024","21/08-2024 -> 28/08-2024", "21/09-2024 -> 28/09-2024"];
 const eventinfo = ["Ordrup gymnasium","UCL Erhvers akademi","Makurs plads"];
 const filename = ["cbs_koebenhavn","dtu_odense","it_universitet_koebenhavn","koebenhavns_universitet","ruc_roskilde","sdu_odense","aalborg_universitet","aarhus_universitet"];
 
