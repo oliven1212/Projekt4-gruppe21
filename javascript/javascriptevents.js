1a67891ebdb76883f3a3210b019811b2944324ea
Valdemar <pizza27@live.dk>
2024-04-11 10:11:09 +0200

    Update javascriptevents.js

--- javascript/javascriptevents.js
+++ javascript/javascriptevents.js
@@ -2,7 +2,7 @@ const uniname = ["Ordrup gymnasium","UCL Erhvers akademi","Makurs plads"];
 const unilocation = ["Kirkevej 5, 2920 Charlottenlund","UCL Erhvers akademi","Makurs plads"];
 const eventtime = ["Ordrup gymnasium","UCL Erhvers akademi","Makurs plads"];
 const eventinfo = ["Ordrup gymnasium","UCL Erhvers akademi","Makurs plads"];
-const filename = ["cbs_koebenhavn","dtu_odense","it_universitet_koebenhavn","koebenhavns_universitet","ruc_roskilde"];
+const filename = ["cbs_koebenhavn","dtu_odense","it_universitet_koebenhavn","koebenhavns_universitet","ruc_roskilde","sdu_odense","aalborg_universitet","aarhus_universitet"];
 
 for (let i = 0; i < uniname.length; i++) {
 document.getElementById("galleri").innerHTML  += '<div class="box"><img src="pictures/events/'+filename[i]+'.png" alt="united"><div class="img-text"><h3>'+uniname[i]+'</h3><p>'+unilocation[i]+'</p><p>'+eventtime[i]+'</p></div><div class="cover"><h3>'+uniname[i]+'</h3><p>'+unilocation[i]+'</p><p>'+eventtime[i]+'</p><p>'+eventinfo[i]+'</p></div></div>';
