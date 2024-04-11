cc296e2a34ca811fd832260129725c2d15064639
JeppeDahlbom <jeppedahlbom@hotmail.com>
2024-04-11 10:30:59 +0200

    s

--- javascript/javascriptevents.js
+++ javascript/javascriptevents.js
@@ -1,5 +1,5 @@
 const uniname = ["Ordrup gymnasium","UCL Erhvers akademi","Makurs plads"];
-const unilocation = ["Kirkevej 5, 2920 Charlottenlund","UCL Erhvers akademi","Makurs plads"];
+const unilocation = ["Solbjerg Pl. 3, 2000 Frederiksberg","Rued Langgaards Vej 7, 2300 København","Nørregade 10, 1172 København","Universitetsvej 1, 4000 Roskilde","Campusvej 55, 5000 Odense","Fredrik Bajers Vej 7K, 9220 Aalborg Øst","Nordre Ringgade 1, 8000 Aarhus C"];
 const eventtime = ["Ordrup gymnasium","UCL Erhvers akademi","Makurs plads"];
 const eventinfo = ["Ordrup gymnasium","UCL Erhvers akademi","Makurs plads"];
 const filename = ["cbs_koebenhavn","dtu_odense","it_universitet_koebenhavn","koebenhavns_universitet","ruc_roskilde","sdu_odense","aalborg_universitet","aarhus_universitet"];
