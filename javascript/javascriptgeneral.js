b1efcc1e888c8744363eb954c5295fc10ba08885
Valdemar <pizza27@live.dk>
2024-04-12 12:36:30 +0200

    Update javascriptgeneral.js

--- javascript/javascriptgeneral.js
+++ javascript/javascriptgeneral.js
@@ -15,7 +15,7 @@ temp = true;
 function burgershow(){
     if(temp){
     temp = false;
-    document.getElementById("header").innerHTML+= '<div class="burger"><a href="index.html" class="burgeritem">Forside</a><a href="effort.html" class="burgeritem">Indsats</a><a href="events.html" class="burgeritem">Begivenheder</a><a href="about.html" class="burgeritem">Om os</a></div>';
+    document.getElementById("header").innerHTML+= '<div class="burger"><a href="index.html" class="burgeritem">Forside</a><a href="map.html" class="burgeritem">Track dit tøj</a><a href="events.html" class="burgeritem">Begivenheder</a><a href="about.html" class="burgeritem">Om os</a></div>';
     }else{
         temp = true;
         phoneheader();
